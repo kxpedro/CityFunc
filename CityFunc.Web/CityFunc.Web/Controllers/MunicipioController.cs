686df3217e5e6f9ae0dd278adf5c57b8c8b38093
kxpedro <pedrorshenrique1515@hotmail.com>
2019-07-19 12:31:07 -0300

    Initial Commit

---
+++ CityFunc.Web/CityFunc.Web/Controllers/MunicipioController.cs
@@ -0,0 +1,55 @@
+﻿using CityFunc.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CityFunc.Web.Controllers
+{
+    public class MunicipioController : Controller
+    {        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _partialSearch()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string text)
+        {
+            try
+            {
+                //SELECT
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+     
+        public ActionResult _partialCreate()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Create(Municipio m)
+        {
+            try
+            {               
+                //INSERT
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        } 
+        
+    }
+}
---
+++ CityFunc.Web/CityFunc.Web/Models/Municipio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CityFunc.Web.Models
+{
+    public class Municipio
+    {
+        public int Id { get; set; }
+        public string UF { get; set; }
+        public string Nome { get; set; }
+    }
+}
